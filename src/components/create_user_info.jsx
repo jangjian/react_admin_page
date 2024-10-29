095756867cba8067fef097380f78fa7e5753a564
jangjian <pureunnabi@gmail.com>
2024-10-29 13:22:12 +0900

    폼 완성

--- src/components/create_user_info.jsx
+++ src/components/create_user_info.jsx
@@ -1,106 +1,166 @@
 import React, { useState } from 'react';
-import { UploadOutlined } from '@ant-design/icons';
+import { InboxOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import {
     Button,
-    Cascader,
-    DatePicker,
     Form,
     Input,
     InputNumber,
     Radio,
     Select,
-    Switch,
-    TreeSelect,
+    Space,
     Upload,
 } from 'antd';
+const { Option } = Select;
 
+const formItemLayout = {
+    labelCol: {
+        span: 9,
+    },
+    wrapperCol: {
+        span: 20,
+    },
+};
+const normFile = (e) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+        return e;
+    }
+    return e?.fileList;
+};
+const onChange = (value) => {
+    console.log('changed', value);
+};
+const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+};
 const UserForm = () => {
-    const [componentSize, setComponentSize] = useState('default');
-
-    const onFormLayoutChange = ({ size }) => {
-        setComponentSize(size);
-    };
-
-    const uploadProps = {
-        action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-        onChange({ file, fileList }) {
-            if (file.status !== 'uploading') {
-                console.log(file, fileList);
-            }
-        },
-    };
+    const [passwordVisible, setPasswordVisible] = useState(false); // 상태 변수 선언
 
     return (
         <Form
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 14,
-            }}
-            layout="horizontal"
-            initialValues={{
-                size: componentSize,
-            }}
-            onValuesChange={onFormLayoutChange}
-            size={componentSize}
+            name="validate_other"
+            {...formItemLayout}
+            onFinish={onFinish}
             style={{
-                maxWidth: 600,
+                maxWidth: 700,
             }}
         >
-            <Form.Item label="Form Size" name="size">
-                <Radio.Group>
-                    <Radio.Button value="small">Small</Radio.Button>
-                    <Radio.Button value="default">Default</Radio.Button>
-                    <Radio.Button value="large">Large</Radio.Button>
-                </Radio.Group>
-            </Form.Item>
-            <Form.Item label="Input">
-                <Input />
-            </Form.Item>
-            <Form.Item label="Select">
-                <Select>
-                    <Select.Option value="demo">Demo</Select.Option>
+            <Form.Item
+                name="인식 유형"
+                label="인식 유형"
+                hasFeedback
+                rules={[
+                    {
+                        required: true,
+                        message: '인식 유형을 선택하세요',
+                    },
+                ]}
+            >
+                <Select placeholder="인식 유형을 선택해주세요">
+                    <Option value="Staff">Staff</Option>
+                    <Option value="visitor">Visitor</Option>
+                    <Option value="blacklist">Blacklist</Option>
                 </Select>
             </Form.Item>
-            <Form.Item label="TreeSelect">
-                <TreeSelect
-                    treeData={[
-                        {
-                            title: 'Light',
-                            value: 'light',
-                            children: [{ title: 'Bamboo', value: 'bamboo' }],
-                        },
-                    ]}
-                />
+
+            <Form.Item label="아이디">
+                <Input />
             </Form.Item>
-            <Form.Item label="Cascader">
-                <Cascader
-                    options={[
-                        {
-                            value: 'zhejiang',
-                            label: 'Zhejiang',
-                            children: [{ value: 'hangzhou', label: 'Hangzhou' }],
-                        },
-                    ]}
-                />
+
+            <Form.Item
+                name="name"
+                label="이름"
+                rules={[
+                    {
+                        required: true,
+                        message: '이름을 입력해주세요',
+                    },
+                ]}
+            >
+                <Input />
             </Form.Item>
-            <Form.Item label="DatePicker">
-                <DatePicker />
+
+            <Form.Item
+                name="id_admin"
+                label="관리자 권한 활성화"
+                rules={[
+                    {
+                        required: true,
+                        message: '관리자 권한 여부를 선택해주세요',
+                    },
+                ]}
+            >
+                <Radio.Group>
+                    <Radio value="활성화">활성화</Radio>
+                    <Radio value="비활성화">비활성화</Radio>
+                </Radio.Group>
             </Form.Item>
-            <Form.Item label="InputNumber">
-                <InputNumber />
+
+            <Form.Item name="password" label="비밀번호">
+                <Space direction="horizontal">
+                    <Input.Password
+                        placeholder="비밀번호를 입력해주세요"
+                        visibilityToggle={{
+                            visible: passwordVisible,
+                            onVisibleChange: setPasswordVisible,
+                        }}
+                        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                    />
+                </Space>
             </Form.Item>
-            <Form.Item label="Switch" valuePropName="checked">
-                <Switch />
+
+            <Form.Item label="사진 업로드">
+                <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                    <Upload.Dragger name="files" action="/upload.do">
+                        <p className="ant-upload-drag-icon">
+                            <InboxOutlined />
+                        </p>
+                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                        <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                    </Upload.Dragger>
+                </Form.Item>
             </Form.Item>
-            <Form.Item label="Upload">
-                <Upload {...uploadProps}>
-                    <Button icon={<UploadOutlined />}>Upload</Button>
-                </Upload>
+
+            <Form.Item label="연락처">
+                <Space direction="horizontal">
+                    <Space.Compact>
+                        <Input
+                            style={{
+                                width: '20%',
+                            }}
+                            defaultValue="010"
+                        />
+                        <Input
+                            style={{
+                                width: '80%',
+                            }}
+                        />
+                    </Space.Compact>
+                </Space>
             </Form.Item>
-            <Form.Item label="Button">
-                <Button type="primary">제출</Button>
+            <Form.Item
+                name="group_list"
+                label="group_list"
+               rules={[
+                   {
+                       required: true,
+                       message: 'group_list를 입력해주세요',
+                   },
+               ]}>
+                <InputNumber min={1} max={10} onChange={onChange} />
+            </Form.Item>
+            <Form.Item
+                wrapperCol={{
+                    span: 12,
+                    offset: 6,
+                }}
+            >
+                <Space>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    <Button htmlType="reset">Reset</Button>
+                </Space>
             </Form.Item>
         </Form>
     );
